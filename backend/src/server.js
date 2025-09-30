2fbda3efab8244fa11822cf3a3ac9010f2e25960
arthurw007BSU <arthur.williamson@bsu.edu>
2025-09-30 01:41:31 +0000

    chore:  db up and running on localhost

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,7 @@
 import "dotenv/config";
 import express from "express";
 import cors from "cors";
+import mongoose from "mongoose";
 
 const app = express();
 
@@ -12,8 +13,23 @@ app.use(express.json());
 app.get("/", (_req, res) => res.send("Recipes API is running"));
 app.get("/healthz", (_req, res) => res.send("ok"));
 
-// env/port
-const PORT = process.env.PORT || 8080;
-app.listen(PORT, () => {
-  console.log(`API listening on http://localhost:${PORT}`);
-});
+// --- MongoDB connect ---
+const MONGO_URL = process.env.MONGO_URL;
+if (!MONGO_URL) {
+  console.error("Missing MONGO_URL in .env");
+  process.exit(1);
+}
+
+mongoose
+  .connect(MONGO_URL)
+  .then(() => {
+    console.log("MongoDB connected");
+    const PORT = process.env.PORT || 8080;
+    app.listen(PORT, () => {
+      console.log(`API listening on http://localhost:${PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.error("❌ Mongo connection error:", err.message);
+    process.exit(1);
+  });
